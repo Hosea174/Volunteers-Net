e4867e3eb83768e5362d0a0b7d8e44c51992cab5
Hosea174 <hossaena2018@gmail.com>
2021-08-29 10:14:47 +0300

    comments added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, jsonify, url_for, Markup
+from flask import Flask, flash, redirect, render_template, request, session, jsonify, Markup
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -16,8 +16,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Ensure responses aren't cached
-
-
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -35,6 +33,8 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL(os.getenv("DATABASE_URL"))
 
+# redirect user to homepage if they are not logged in
+
 
 def login_required(f):
     @wraps(f)
@@ -45,14 +45,30 @@ def login_required(f):
     return decorated_function
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    # subscribe to newsletter
+    if request.method == 'POST':
+        # get the user's email
+        email = request.form.get('email')
+        emails = [em['email']
+                  for em in db.execute('SELECT email FROM subscribers')]
+        if email:
+            # check to see if the user is already subscribed before adding the email to database
+            if email in emails:
+                flash("You are already subscribed to our Mailing list " +
+                      u'\U0001F525', "info")
+            else:
+                db.execute('INSERT INTO subscribers (email) VALUES (?)', email)
+                flash("Thanks for Subscribing! " + u'\U0001F64C', 'info')
+            return redirect('/')
     return render_template('index.html')
 
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
     if request.method == 'POST':
+        # get the user's info
         username = request.form.get("username")
         email = request.form.get("email")
         password1 = request.form.get("password1")
@@ -63,6 +79,7 @@ def signup():
         else:
             company = None
 
+        # Validate inputs
         if db.execute("SELECT username FROM users WHERE username=?", username):
             flash("User name Is Taken", "danger")
             return redirect('/signup')
@@ -74,16 +91,17 @@ def signup():
             flash("Passwords Don't Match", "danger")
             return redirect('/signup')
 
+        # add the user's input to a database
         password = generate_password_hash(password1)
         db.execute("INSERT INTO users (username, email, password, mode, company) VALUES (?, ?, ?, ?, ?)",
                    username, email, password, mode, company)
-
+        # Set session keys. 'user_id' is the id of the user in the db
+        # and 'user_mode' tells if the user is signed up as a 'recruiter' or a 'volunteer'
         user_id = db.execute(
             "SELECT id FROM users WHERE username=?", username)[0]['id']
         session["user_id"] = user_id
         session["user_mode"] = db.execute(
             "SELECT mode FROM users WHERE username=?", username)[0]['mode']
-
         return redirect('/')
     else:
         return render_template('signup.html')
@@ -93,20 +111,27 @@ def signup():
 def login():
 
     if request.method == 'POST':
+        # get user info for login
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
+        # validate user input
+        if not username or not email or not password:
+            flash("Please fill out the boxes below", "danger")
+            return redirect('/login')
+        # query the row of the user
         row = db.execute(
             "SELECT * FROM users WHERE username=? AND email=?", username, email)
 
-        # if a user is not found in the table that has the given username, email and password
+        # if a user is not found in the table with username, email and password or if the password is incorrect
         if len(row) != 1 or not check_password_hash(row[0]['password'], password):
             flash("incorrect Username and password combination", "danger")
             return redirect('/login')
 
         # clear any old session
         session.clear()
-        # store the logged in user's id in session
+
+        # store the new user's id in session
         session['user_id'] = row[0]['id']
         session['user_mode'] = row[0]['mode']
         return redirect('/')
@@ -121,40 +146,28 @@ def logout():
     return redirect('/')
 
 
-@app.route('/subscribe', methods=['POST'])
-def subscribe():
-    email = request.form.get('email')
-    emails = [em['email']
-              for em in db.execute('SELECT email FROM subscribers')]
-    if email:
-        if email in emails:
-            flash("You are already subscribed to our Mailing list " +
-                  u'\U0001F525', "info")
-        else:
-            db.execute('INSERT INTO subscribers (email) VALUES (?)', email)
-            flash("Thanks for Subscribing! " + u'\U0001F64C', 'info')
-
-    return redirect('/')
-
-
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    '''contact us page'''
     if request.method == 'POST':
-        # sender
+        # get sender's info
         email = request.form.get('email')
         password = request.form.get('password')
         subject = request.form.get('subject')
         message = request.form.get('message')
-        message = 'Subject: {}\n\n{}'.format(subject, message)
+        message = 'Subject: {}\n\nYou have recieved a new email from.{}\n{}'.format(
+            subject, email, message)
 
-        # send the message to admin
-        # THIS IS JUST A TEMPORARY EMAIL FOR TESTING THINGS
+        # send the message to the admin as if it was sent from a single common email
+        # THIS IS JUST A TEMPORARY EMAIL
         admin = "xijoyak250@186site.com"
+        common_sender_email = "temp_mail"
+        common_sender_password = "1234"
         try:
             server = smtplib.SMTP("smtp.gmail.com", 587)
             server.starttls()
-            server.login(email, password)
-            server.sendmail(email, admin, message)
+            server.login(common_sender_email, common_sender_password)
+            server.sendmail(common_sender_email, admin, message)
             server.quit()
         except:
             flash("Couldn't send email, try again", "danger")
@@ -166,6 +179,7 @@ def contact():
 @app.route('/volunteer-form', methods=['GET', 'POST'])
 @login_required
 def vform():
+    '''volunteer form'''
     user_id = session['user_id']
     if request.method == 'POST':
         # get data from the volunteers form
@@ -184,11 +198,12 @@ def vform():
         # check to see if the user has already submitted the form
         exists = db.execute(
             "SELECT * FROM applications WHERE applicant_id=?", user_id)
-
         if any(exists) and exists[0]:
             flash(Markup(
                 "You have already submitted this form, wait till you get <a href='/offers'>Offers</a>" + u'\U0001f609'), "info")
             return redirect('/')
+
+        # validate input
         if not days:
             flash("You have to select at least 1 day", "info")
             return redirect('/volunteer-form')
@@ -198,6 +213,8 @@ def vform():
         if other:
             activities.append(other)
         activities = [a for a in activities if a != 'other']
+        # add the info to applications table
+        # I converted 'days' and 'activities' to string so that I can store a list of values in a single cell
         db.execute(
             "INSERT INTO applications (applicant_id, fname, lname, age, phone, country, city, street1, street2, days, activities) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             user_id, fname, lname, age, phone, country, city, street1, street2, str(days), str(activities))
@@ -214,25 +231,28 @@ def vform():
 @app.route('/search')
 @login_required
 def search():
+    '''Filter and search volunteers data'''
+    # if the user is signed in as a volunteer, redirect them to the homepage because only a recruiter can access this page
     current_row = db.execute(
         'SELECT mode FROM users WHERE id = ?', session['user_id'])
     if current_row[0]['mode'] == 'volunteer':
         return redirect('/')
+    # get the provided parameters
     country = request.args.get("country")
     city = request.args.get("city")
     age = request.args.get("age")
-    if age == '40+':
+    if age and age == '40+':
         age = ['40', '200']
     elif age:
         age = age.split('-')
-
     days = request.args.getlist("days")
     activities = request.args.getlist("activities")
-
     other = request.args.get("other-text")
     if other:
         activities.append(other.lower())
+
     if country and city and age and days and activities:
+        # search the database with the given inputs
         search_results = db.execute("SELECT * FROM applications WHERE country = ? AND city = ? AND age BETWEEN ? AND ? AND days = ? AND activities = ?",
                                     country, city, age[0], age[1], str(days), str(activities))
 
@@ -251,12 +271,15 @@ def search():
 
 @app.route('/request_volunteer', methods=['POST'])
 def request_volunteer():
+    '''Send a request to a volunteer'''
+    # get the id and username of the volunteer to whom the request is being sent
     applicant_id = request.get_json()[0]['applicant_id']
-    recruiter_name = db.execute(
-        "SELECT username FROM users WHERE id=?", session['user_id'])[0]['username']
     volunteer_name = db.execute(
         "SELECT username FROM users WHERE id=?", applicant_id)[0]['username']
+    recruiter_name = db.execute(
+        "SELECT username FROM users WHERE id=?", session['user_id'])[0]['username']
     try:
+        # append the requester(the recruiter), the 'volunteer' and the status i.e. pending, accepted, rejected
         db.execute(
             "INSERT INTO offers (user_id, recruiter, volunteer, status) VALUES (?, ?, ?, ?)", applicant_id, recruiter_name, volunteer_name, "pending")
         results = {'processed': 'true'}
@@ -268,21 +291,25 @@ def request_volunteer():
 @login_required
 @app.route('/offers', methods=['POST', 'GET'])
 def offers():
+    '''Displays all the offers that the volunteer has received'''
     username = db.execute(
         "SELECT username FROM users WHERE id=?", session['user_id'])[0]['username']
     if request.method == "POST":
+        # update the status of 'offers' table based on the data sent from AJAX
         recruiter = request.get_json()[0]['recruiter']
         response_id = request.get_json()[1]['response_id']
         db.execute("UPDATE offers SET status=? WHERE volunteer=? AND recruiter=?",
                    response_id, username, recruiter)
         return jsonify({'status': response_id})
     else:
+        # get all the offers that the volunteer received
         offers_for_volunteer = db.execute(
             "SELECT recruiter, status FROM offers WHERE volunteer=?", username)
         if not any(offers_for_volunteer):
             msg = f"You haven't received any offers yet."
             return render_template('offers.html', msg=msg)
         for offer in offers_for_volunteer:
+            # add the recruiter's email and company name to 'offers_for_volunteer'
             email = db.execute(
                 'SELECT email FROM users WHERE username=?', offer['recruiter'])[0]['email']
             company = db.execute(
@@ -295,6 +322,7 @@ def offers():
 @login_required
 @app.route('/requests')
 def requests():
+    '''Displays all the requests made by the recruiter'''
     recruiter = db.execute(
         "SELECT username FROM users WHERE id=?", session['user_id'])[0]['username']
     requested_offers = db.execute(
@@ -302,6 +330,7 @@ def requests():
     if not any(requested_offers):
         msg = "You haven't made any requests yet"
         return render_template("requested.html", msg=msg)
+    # get the whole info about each volunteers that the recruiter sent requests to
     for offer in requested_offers:
         vol_info = db.execute(
             "SELECT applicant_id, fname, lname, age, phone, country, city, street1, days, activities FROM applications WHERE applicant_id=?", offer['user_id'])[0]
